d644d5565a4033fbbf5f390d8ae0425aafd2ab07
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-02-11 09:49:08 +0100

    Result in 3 algorithms Brutal Force, Sorting and Hash

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,19 +1,39 @@
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-def containsDuplicate(nums):
+
+def containsDuplicateBrutalForce(nums):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return print(f"The array contains duplicates: {nums[i]} == {nums[j]}")
+        print("The array does not contain duplicates.")
+
+def containsDuplicateSorting(nums):
+        nums.sort()
+        for i in range (len(nums) - 1):
+            if nums[i] == nums[i+1]:
+                return print(f"The array contains duplicates: {nums[i]} == {nums[i+1]}")
+        print("The array does not contain duplicates.")
+
+def containsDuplicateHashSet(nums):
         hashset = set()
 
         for n in nums:
             if n in hashset:
-                return True
+                return print(f"The array contains duplicates: {n} == {n}")
             hashset.add(n)
-        return False
+        print("The array does not contain duplicates.")
 
+while True:
+    nums = list(map(int, input("Enter the elements of the array, separated by spaces: ").strip().split()))
+    algorithm = int(input("Which algorithm do you want to use:\n1. Brutal force?\n2. Sorting?\n3. Hashset?\nChoose the number: "))
 
-nums = list(map(int, input("Enter the elements of the array, separated by spaces: ").strip().split()))
-result = containsDuplicate(nums)
-if result:
-    print("The array contains duplicates.")
-else:
-    print("The array does not contain duplicates.")
+    if algorithm == 1:
+        containsDuplicateBrutalForce(nums)
+    elif  algorithm == 2:
+        containsDuplicateSorting(nums)
+    elif  algorithm == 3:
+        containsDuplicateHashSet(nums)
+    else:
+        print("Valid input. Choose one more time.")
 
 
